90dff98c0bd192a4ecd1757e68c4d68d78fe05cb
Jufebrown <jufebrown@gmail.com>
2018-11-05 14:37:13 -0600

    add logic for add xp button

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -31,9 +31,10 @@ namespace WPFUI
             DataContext = _gameSession;
         }
 
-        private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-
+            _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
         }
+
     }
 }
