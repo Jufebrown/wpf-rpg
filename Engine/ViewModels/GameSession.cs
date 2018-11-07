f9087c18329b2184fa966629b97d49680478ace1
Jufebrown <jufebrown@gmail.com>
2018-11-07 10:18:02 -0600

    set starting location to player home

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -90,7 +90,7 @@ namespace Engine.ViewModels
 
             CurrentWorld = WorldFactory.CreateWorld();
 
-            CurrentLocation = CurrentWorld.LocationAt(0, 2);
+            CurrentLocation = CurrentWorld.LocationAt(0, -1);
         }
 
         public void MoveNorth()
