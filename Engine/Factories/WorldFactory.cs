9fe157885f90d1c560ac91b2896c8df428bba017
Jufebrown <jufebrown@gmail.com>
2018-11-08 15:05:16 -0600

    add ternary for removing multiple items from inventory

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -9,7 +9,6 @@ namespace Engine.Factories
             World newWorld = new World();
 
             newWorld.AddLocation(-2, -1, "Farmer's Field", "There are rows of corn growing here, with giant rats hiding between them.", "FarmFields.png");
-
             newWorld.LocationAt(-2, -1).AddMonster(2, 100);
 
             newWorld.AddLocation(-1, -1, "Farmer's House", "This is the house of your neighbor, Farmer Ted.", "Farmhouse.png");
@@ -25,7 +24,6 @@ namespace Engine.Factories
             newWorld.AddLocation(1, 0, "Town Gate", "There is a gate here, protecting the town from giant spiders.", "TownGate.png");
 
             newWorld.AddLocation(2, 0, "Spider Forest", "The trees in this forest are covered with spider webs.", "SpiderForest.png");
-
             newWorld.LocationAt(2, 0).AddMonster(3, 100);
 
             newWorld.AddLocation(0, 1, "Herbalist's Hut", "You see a small hut with plants drying from the roof.", "HerbalistsHut.png");
@@ -33,7 +31,6 @@ namespace Engine.Factories
             newWorld.LocationAt(0, 1).TraderHere = TraderFactory.GetTraderByName("Pete the Herbalist");
 
             newWorld.AddLocation(0, 2, "Herbalist's Garden", "There are many plants here with snakes hiding behind them.", "HerbalistsGarden.png");
-
             newWorld.LocationAt(0, 2).AddMonster(1, 100);
 
             return newWorld;
